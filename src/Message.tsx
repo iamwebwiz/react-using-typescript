db8def116e338ad33de4e791b7016c0bbf3d8723
Ezekiel <iamwebwiz@gmail.com>
2020-07-04 03:48:09 +0100

    Update Message component for high-order components

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import messageHoc from './Hoc'
 
 interface UserMessage {
     name: string,
     message: string
 }
 
-const Message = (props: any): any => <p>Hi {props.name}, {props.message}</p>
+const example = (props: any): any => <p>Hi {props.name}, {props.message}</p>
+
+const Message = messageHoc(example)
 
 export default Message
