3e5579908b3c5b42a42eebeed4b9525ed883e780
Ezekiel <iamwebwiz@gmail.com>
2020-07-04 01:44:47 +0100

    Change function App component to class/stateful component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import logo from './logo.svg'
 import './App.css'
 import Message from './Message'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message message="This is a simple message" />
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message message="This is a simple message" />
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
