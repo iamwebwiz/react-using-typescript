6e30e951867517ae666c27f09377723a9a3d3012
Ezekiel <iamwebwiz@gmail.com>
2020-07-04 03:48:40 +0100

    Refactor App.tsx for higher-order components (HOCs)

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,13 @@ import logo from './logo.svg'
 import './App.css'
 import Message from './Message'
 
-const initialState = {
-    name: 'Ezekiel',
-    message: 'TypeScript is cool'
-}
-
-type State = Readonly<typeof initialState>
-
-class App extends React.Component<any, State> {
-    readonly state: State = initialState
-
-    componentDidMount() {
-        console.log('The App component mounted as expected!')
-    }
-
+class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <Message message={this.state.message} name={this.state.name} />
+                    <Message />
                 </header>
             </div>
         )
