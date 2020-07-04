7eaf6068f20bf85c1338c930b551a04eb5621c84
Ezekiel <iamwebwiz@gmail.com>
2020-07-04 03:04:55 +0100

    HoC inside React

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,28 @@ import logo from './logo.svg'
 import './App.css'
 import Message from './Message'
 
-class App extends React.Component<any> {
+const initialState = {
+    name: 'Ezekiel',
+    message: 'TypeScript is cool'
+}
+
+type State = Readonly<typeof initialState>
+
+const messageHoc = (WrappedComponent: any) => {
+    class HOC extends React.Component<{}, State> {
+        readonly state: State = initialState
+
+        render() {
+            return (
+                <WrappedComponent name={this.state.name} message={this.state.message} />
+            )
+        }
+    }
+}
+
+class App extends React.Component<any, State> {
+    readonly state: State = initialState
+
     componentDidMount() {
         console.log('The App component mounted as expected!')
     }
@@ -13,7 +34,7 @@ class App extends React.Component<any> {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <Message message="welcome to TypeScript Universe!!" name='Ezekiel'/>
+                    <Message message={this.state.message} name={this.state.name} />
                 </header>
             </div>
         )
