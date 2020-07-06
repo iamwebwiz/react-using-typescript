53dbfb141199796c559151786f5fb19980fb5b89
Ezekiel <iamwebwiz@gmail.com>
2020-07-06 02:48:15 +0100

    Refactor class to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import logo from './logo.svg'
 import './App.css'
 import Message from './Message'
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <Message />
-                </header>
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Message />
+            </header>
+        </div>
+    )
 }
 
 export default App
